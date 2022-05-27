59afd8a629998cfc0bcae999f24b694407bea67e
eojin <eojinn87@gmail.com>
2022-05-27 16:09:43 +0900

    androidproject0527_3

---
+++ app/src/main/java/kr/ac/yeonsung/androidproject0527_3/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yeonsung.androidproject0527_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
